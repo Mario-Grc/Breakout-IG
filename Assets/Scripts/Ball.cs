7eda0f62f0d846098c2c5bffbfa79f8096db794b
Mario-Grc <mariotrabajos23@gmail.com>
2026-01-01 19:20:52 +0000

    Fix speed bug

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public float speed = 5f;
+    public float minVerticalSpeed = 1.0f;
     private Rigidbody2D rb;
     public GameOverManager gameOverManager;
     private Transform playerTransform;
@@ -11,14 +12,24 @@ public class Ball : MonoBehaviour
     {
         gameOverManager = GameObject.FindGameObjectWithTag("GameOverManager").GetComponent<GameOverManager>();
         rb = GetComponent<Rigidbody2D>();
-        rb.linearVelocity = new Vector2(2f, -2f).normalized * speed;
+        rb.linearVelocity = new Vector2(0f, -2f).normalized * speed;
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void FixedUpdate()
     {
+        Vector2 vel = rb.linearVelocity;
+
+        // para asegurarnos de que la pelota no se queda atascada
+        if (Mathf.Abs(vel.y) < minVerticalSpeed) {
+            // Forzar un componente vertical m�nimo
+            vel.y = minVerticalSpeed * Mathf.Sign(vel.y);
+            // Si y era 0, forzar hacia abajo
+            if (vel.y == 0) vel.y = -minVerticalSpeed;
+        }
+
         // Mantener velocidad constante
-        rb.linearVelocity = rb.linearVelocity.normalized * speed;
+        rb.linearVelocity = vel.normalized * speed;
 
         if (playerTransform != null && transform.position.y < playerTransform.position.y)
         {
--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -3,13 +3,21 @@ using UnityEngine;
 public class PaddleController : MonoBehaviour
 {
     public float speed = 10f;
+    public float minx = -9.1f;
+    public float maxx = 9.1f;
     public GameObject gameOverScreen;
 
     void Update()
     {
         if (!gameOverScreen.activeSelf) { 
             float horizontal = Input.GetAxis("Horizontal");
-            transform.position += Vector3.right * horizontal * speed * Time.deltaTime;
+
+            Vector3 pos = transform.position;
+
+            pos.x += horizontal * speed * Time.deltaTime;
+            pos.x = Mathf.Clamp(pos.x, minx, maxx);
+
+            transform.position = pos;
         }
     }
 }
